c471b4f368d6771ef97d6721b2764b9981eead8f
Adib Ahsan <adibahsan@iut-dhaka.edu>
2021-12-21 13:02:20 +0600

    initial stylings

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 
 export default function App() {
   return (
     <View
       style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        padding : 30
       }}
     >
-      <Text>Universal React with Expo</Text>
+        <View>
+            <TextInput placeholder = "Course Goal" style = {{
+                borderColor : "black",
+                borderWidth : 1,
+                padding : 10
+            }}/>
+            <Button title = "Henlo"/>
+        </View>
+      <Text>Universal React Native with React</Text>
     </View>
   );
 }
