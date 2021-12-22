f3a265cd6d26b37c5d51a9f108da9a6cd280e03a
Adib Ahsan <adibahsan@iut-dhaka.edu>
2021-12-22 22:55:21 +0600

    stylesheet added

--- App.js
+++ App.js
@@ -1,30 +1,31 @@
-import * as React from "react";
-import {Button, Text, TextInput, View} from "react-native";
+import * as React from 'react';
+import {Button, Text, TextInput, View, StyleSheet} from 'react-native';
 
-export default function App() {
-
-    return (
-        <View
-            style={{
-                padding: 30
-            }}
-        >
-            <View style = {
-                {
-                    flexDirection : "row",
-                    justifyContent : "space-between",
-                    alignItems : "center"
-                }
-            }>
-                <TextInput placeholder="Course Goal" style={{
-                    borderColor: "black",
-                    borderWidth: 1,
-                    padding: 10,
-                    width : "80%",
-                }}/>
-                <Button title="Henlo"/>
-            </View>
-            <Text>Universal React Native with React</Text>
-        </View>
-    );
+export default function App () {
+  return (
+    <View style={styles.screen}>
+      <View style={styles.inputPlaceHolder}>
+        <TextInput placeholder="Course Goal" style={styles.input} />
+        <Button title="Henlo" />
+      </View>
+      <Text>Universal React Native with React</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create ({
+  screen: {
+    padding: 30,
+  },
+  inputPlaceHolder: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+    width: '80%',
+  },
+});
